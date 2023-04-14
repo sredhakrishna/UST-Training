3352368d336c134f69766ab3dcb510b5b9049842
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-04-14 11:46:57 +0530

    Add files via upload

---
+++ Day 1/Helloworld/Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+namespace Helloworld
+{
+    class one
+    { 
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+         
+        }
+        
+    }
+}
