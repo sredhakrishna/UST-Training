c40976e21f3e9e158d169ed5e1cf984c2437e5fb
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-04-14 16:55:15 +0530

    Add files via upload

---
+++ Day9/Mail/Mail.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+
+namespace Mail
+{
+    internal class Mail
+    {   
+        private long _id;
+        private string _to;
+        private string _from;
+        private string _subject;
+        private string _content;
+       // private DateTime _recieved;
+        private double _size;
+        
+        public long Id
+        {
+            get
+            {
+                return _id;
+
+            }
+            set
+            {
+                _id = value; 
+            }
+
+        }
+
+        public string To
+        {
+            get
+            {
+                return _to;
+
+            }
+            set
+            {
+                _to = value;
+            }
+
+        }
+        public string From
+        {
+            get
+            {
+                return _from;
+
+            }
+            set
+            {
+                _from = value;
+            }
+
+        }
+
+        public string Subject
+        {
+            get
+            {
+                return _subject;
+
+            }
+            set
+            {
+                _subject = value;
+            }
+
+        }
+        public string  Content
+        {
+            get
+            {
+                return _content;
+
+            }
+            set
+            {
+                _content= value;
+            }
+
+        }
+
+
+     /*   public DateTime RecievedDate
+        {
+            get
+            {
+                return _recieved;
+
+            }
+            set
+            {
+                _recieved = value;
+            }
+
+        }
+     */
+        public double Size
+        {
+            get
+            {
+                return _size;
+
+            }
+            set
+            {
+                _size = value;
+            }
+
+        }
+        public  Mail()
+        {
+
+        }
+        public Mail(long _id, string _to, string _from, string _subject, string _content,  double _size)
+        {
+            Id= _id;
+            To= _to;
+            From= _from;
+            Subject= _subject;
+            Content= _content;
+            //RecievedDate= _receivedDate;
+            Size= _size;
+        }
+        public override string ToString()
+        {
+            return "Id:" + +Id + "\n" + " To" + To + "\n";
+        }
+
+
+    }
+}
---
+++ Day9/Mail/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Mail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter _id, _to,  _from,  _subject,  _content,  _receivedDate,  _size with comas");
+            string input=Console.ReadLine();
+            string[] arrinput=input.Split(',');
+            long id =long.Parse(arrinput[0]);
+            string to = arrinput[1];
+            string from = arrinput[2];  
+            string subject = arrinput[3];
+            string content = arrinput[4];
+            //DateTime receivedDate = DateTime.Parse(arrinput[5]);
+            double size = long.Parse(arrinput[5]);
+
+
+            Console.WriteLine("enter second _id, _to,  _from,  _subject,  _content,  _receivedDate,  _size with comas");
+            string input1 = Console.ReadLine();
+            string[] arrinpu1t = input.Split(',');
+            long id1 = long.Parse(arrinput[0]);
+            string to1 = arrinput[1];
+            string from1 = arrinput[2];
+            string subject1 = arrinput[3];
+            string content1 = arrinput[4];
+           // DateTime receivedDate1 = DateTime.Parse(arrinput[5]);
+            double size1 = long.Parse(arrinput[5]);
+
+            
+            Mail e1= new Mail(id,to,from,subject,content,size);
+            Mail e2= new Mail(id1,to1,from1,subject1,content1,size1);
+            Console.WriteLine(e1.ToString());
+            Equals(id, id1, to, to1, from, from1);
+
+        }
+        public static void Equals(long id,long id1, string to,string to1,string from,string from1)
+        {
+            if (id == id1 && to == to1 && from == from1)
+            {
+                Console.WriteLine("mail 1 and mail 2 are equal");
+            }
+            else { Console.WriteLine("mail1 and mail2 are not equal"); }
+        }
+    }
+}
