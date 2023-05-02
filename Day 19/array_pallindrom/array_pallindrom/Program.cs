3cd6a9da4dedf84a48752f1647a67300759923ad
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-02 21:35:50 +0530

    Add files via upload

---
+++ Day 19/array_pallindrom/array_pallindrom/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_pallindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 111, 224, 333 };
+            int result = check(a);
+            Console.WriteLine(result);
+        }
+        static int check(int[] a)
+        {
+            int result = 0;
+            int count = 0;
+            int[] b = new int[a.Length];
+            
+            for(int i=0; i<a.Length; i++)
+            {
+                int c= a[i];
+                
+                int a1 = 0;
+                while (a[i] > 0)
+                {
+                    a1 = a1 * 10 + a[i] % 10;
+
+                    a[i] = a[i] / 10;
+                }
+                    if (c == a1)
+                    {
+                    count++;
+                    }
+                } 
+            
+            if (count==a.Length)
+            {
+                result=1;
+            }
+            return result;
+        }
+    }
+}
---
+++ Day 19/binary num/binary num/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "210";
+            int result = check(str);
+            Console.WriteLine(result);
+        }
+
+        private static int check(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]=='0' || str[i]=='1')
+                {
+                    count++;
+                }
+                if (count == str.Length)
+                {
+                    return 1;
+                }
+
+                
+            }
+            return 0;
+
+        }
+    }
+}
