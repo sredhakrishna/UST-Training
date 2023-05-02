ef6a319b7f325ce7c01e1e4ff5e966ae42806196
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-02 17:19:55 +0530

    Add files via upload

---
+++ Day 18/sortedlist/sortedlist/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortedlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s=new SortedList();
+            s.Add("sredha", "2000");
+            s.Add("manjima", "3000");
+            s.Add("anusree", "4000");
+            foreach (var item in s.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in s.Values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
