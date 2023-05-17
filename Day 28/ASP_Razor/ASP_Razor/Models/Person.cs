3a770d9fa2e542dddde25c707d593acff626a5e6
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-17 09:46:20 +0530

    Add files via upload

---
+++ Day 28/ASP_Razor/ASP_Razor/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace ASP_Razor.Models
+{
+    public class Person
+    {
+        public string ? Name { get; set; }   
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+       
+    }
+}
---
+++ Day 28/Assync_await/Assync_await/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Method0();
+        Method1();
+        Method2();
+        Console.ReadKey();
+    }
+    public static async Task Method0()
+    {
+        await Task.Delay(10000);
+        for(int i = 0; i < 101; i++) 
+        {
+            Console.WriteLine(i);
+        }
+    }
+    public static async Task Method1()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(" Method 1");
+                // Do something
+                Task.Delay(100).Wait();
+            }
+        });
+    }
+
+
+    public static void Method2()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(" Method 2");
+            // Do something
+            Task.Delay(100).Wait();
+        }
+    }
+     
+}
