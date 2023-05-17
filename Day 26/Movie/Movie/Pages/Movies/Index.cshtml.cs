d26b8625524975d9409f1224d4e1082984b4b373
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-17 09:36:50 +0530

    Add files via upload

---
+++ Day 26/Movie/Movie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Movie.Data;
+using Movie.Model;
+
+namespace Movie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Movie.Data.MovieContext _context;
+
+        public IndexModel(Movie.Data.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
