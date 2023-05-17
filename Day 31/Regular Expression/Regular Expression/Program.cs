1fbedc5efc03e4d88a3bd8da0a770f54f905a837
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-17 09:49:24 +0530

    Add files via upload

---
+++ Day 31/Regular Expression/Regular Expression/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(@"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}");
+            //class Regex Repesents an immutable regular expression.
+
+            string[] str = { "+91-9678967101", "9678967101", "+91-9678-967101", "+91-96789-67101", "+919678967101" };
+            //Input strings for Match valid mobile number.
+
+            foreach (string s in str)
+            {
+                Console.WriteLine("{0} {1} a valid mobile number.", s,
+                    r.IsMatch(s) ? "is" : "is not");
+                //The IsMatch method is used to validate a string or
+                //to ensure that a string conforms to a particular pattern.
+            }
+        }
+    }
+}
---
+++ Day 31/regex/regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "sredha krishna";
+
+            string p = "@^[aeiouAEIOU]+$";
+            if (Regex.IsMatch(s, p) == true)
+            {
+                Regex.Replace(s, p, "3");
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
