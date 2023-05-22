3937b54ce9238900736aa86724225e56ce26b899
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-22 14:10:53 +0530

    Add files via upload

---
+++ Day 36/practice/practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a value");
+            long a=long.Parse(Console.ReadLine());
+            chrcking(a);
+        }
+        public static void chrcking(long a) {
+            string s = a.ToString();
+            long sum = 0;
+            for(int i = 0; i < s.Length; i++) {
+                if (s[i] %2!= 0) {
+                    sum = sum + s[i];
+                }
+            }
+            if(sum%2!= 0)
+            {
+                Console.WriteLine("sum is odd");
+            }
+            else { Console.WriteLine("sum is even"); }
+        }
+    }
+}
---
+++ Day 36/practise 2/practise 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "malayalam";
+            int b = checking(s);
+            if (b == 1)
+            {
+                Console.WriteLine("valid");
+
+            }
+            else { Console.WriteLine("not valid"); }
+        }
+        public static int checking(string s)
+        {
+            if (s[0] == s[s.Length - 1])
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
