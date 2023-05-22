f135feb8111718e3ae2882f085e73be455735336
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-22 14:07:50 +0530

    Add files via upload

---
+++ Day 34/Singleton design pattern/Singleton design pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromTeachaer = Singleton.GetInstance();
+            fromTeachaer.PrintDetails("From Teacher");
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 34/regex/regex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hey, im here";
+
+            string s2 = Regex.Replace(s, "[^A-Za-z\\s]", "");
+            Console.WriteLine(s2);
+            string[] s3 = s2.Split(' ');
+            double l= s3.Length;
+            Console.WriteLine(l);
+            string s5 = Regex.Replace(s, "[^A-Za-z]", "");
+            Console.WriteLine(s5);
+            double l2 = s5.Length;
+            Console.WriteLine(l2);
+            double AVG = l2 / l;
+            Console.WriteLine(AVG);
+            
+        }
+    }
+}
