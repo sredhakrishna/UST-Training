db236801a02bc91b21f2af11e5bbcd9a741ffc6d
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-22 14:09:21 +0530

    Add files via upload

---
+++ Day 35/3sum/3sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -1, 0, 1, 2, -1, -4 };
+           List<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+
+                    for (int k = 0; k < nums.Length; k++)
+
+                    {
+
+                        if (nums[i] != nums[j] &&nums[ i] !=nums[ k] &&nums[ k] != nums[j])
+                        {
+
+                            if (nums[i] + nums[j] + nums[k] == 0)
+                            {
+
+                                list.Add(new int(nums[i], nums[j], nums[k]));
+                               ;
+
+                                
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+            foreach (int i in list)
+            {
+                Console.WriteLine(list);
+            }
+
+        }
+    }
+}
---
+++ Day 35/Swapping/Swapping/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Sredha";
+            char[]b=a.ToCharArray();
+            char temp ;
+            int i = 0;
+           while(i<b.Length )
+            {
+                temp = b[i];
+                b[i] = a[i+1];
+                b[i+1] = temp;
+                i = i + 2;
+
+            }
+
+            string str = "";
+            for(int k=0; k<b.Length; k++)
+            {
+                str=str + b[k];
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
