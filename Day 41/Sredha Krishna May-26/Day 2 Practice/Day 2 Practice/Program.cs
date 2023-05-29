3de239443835991fc3584983e7155a4e9f34c0bd
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-29 09:30:47 +0530

    Add files via upload

---
+++ Day 41/Sredha Krishna May-26/Day 2 Practice/Day 2 Practice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string str1=Console.ReadLine();
+            Console.WriteLine("enter anagram");
+            string anagram=Console.ReadLine();
+            check(str1,anagram);
+        }
+        public static void check(string str1, string anagram)
+        {
+            str1 = str1.ToLower();
+            anagram = anagram.ToLower();
+
+            char[] arr1 = str1.ToCharArray();
+            char[] arr2 = anagram.ToCharArray();
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    Console.WriteLine("anagram");
+                    break;
+                }
+                else {
+                    Console.WriteLine("not anagram");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Day 41/Sredha Krishna May-26/abdtract/abdtract/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+
+
+
+ internal class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        MyBook book = new MyBook();
+
+        Console.WriteLine("Enter the book name:");
+
+        string s = Console.ReadLine();
+
+        book.SetTitle(s);
+
+        Console.WriteLine("The book title is: " + book.GetTitle());
+
+    }
+
+}
+
+abstract class Book
+
+{
+
+    protected string title;
+
+    public abstract void SetTitle(string s);
+
+    public string GetTitle()
+
+    {
+
+        return title;
+
+    }
+
+}
+
+class MyBook : Book
+
+{
+
+    public override void SetTitle(string s)
+
+    {
+
+        title = s;
+
+    }
+
+}
+
+
+
+
+
