7647bc7f8b8804b4b7d43cfc119b4b9637f7a775
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-29 09:27:45 +0530

    Add files via upload

---
+++ Day 39/Exam qn/Exam qn/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_qn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>()
+            {
+                1, 27,36,21
+            };
+            int result = sol(list);
+            Console.WriteLine(result);
+        }
+
+        private static int sol(List<int> list)
+        {
+            int score = 0;
+           
+            for(int i=0; i<list.Count-1; i++)
+            {
+                if ((list[i]+list[i+1])%2==0) 
+                {
+                    score += 5;
+                }
+            }
+            for (int i = 0; i < list.Count - 2; i++)
+            {
+                if ((list[i] + list[i + 1] + list[i+2]) % 2 != 0&& (list[i] * list[i + 1] * list[i + 2]) % 2==0)
+                {
+                    score += 10;
+                }
+            }
+            return score;
+        }
+    }
+}
---
+++ Day 39/practice 1/practice 1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter student type");
+            string str=Console.ReadLine();
+            if (str == "MSH")
+            {
+                Console.WriteLine("Enter tuition fee");
+                float tf=float.Parse(Console.ReadLine());
+                Console.WriteLine("enter bus fee");
+                float bf=float.Parse(Console.ReadLine());
+                Console.WriteLine("enter hostel fee");
+                float hf = float.Parse(Console.ReadLine());
+                float total = tf + hf;
+                Console.WriteLine("The fees to be paid by the student is Rs:" + $"{total:0.00}");
+                //or Console.WriteLine("The fees to be psid is:"+total.Tostring("0.00"));
+                
+            }
+            if (str == "MSDS")
+            {
+                Console.WriteLine("Enter tuition fee");
+                float tf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter hostel fee");
+                float bf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter hostel fee");
+                float hf = float.Parse(Console.ReadLine());
+                float total = tf + bf;
+                Console.WriteLine("The fees to be paid by the student is Rs:" +$"{ total:0.00}");
+            }
+            if (str == "MGSDS")
+            {
+                Console.WriteLine("Enter tuition fee");
+                float tf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter bus fee");
+                float bf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter hostel fee");
+                float hf=float.Parse(Console.ReadLine());
+                float total = ((150*tf)/100) + bf;
+                Console.WriteLine("The fees to be paid by the student is Rs{0:0.00}" + $"{total:0.00}");
+            }
+            if (str == "MGSH")
+            {
+                Console.WriteLine("Enter tuition fee");
+                float tf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter bus fee");
+                float bf = float.Parse(Console.ReadLine());
+                Console.WriteLine("enter hostel fee");
+                float hf = float.Parse(Console.ReadLine());
+                float total = ((150 * tf) / 100) + hf;
+                Console.WriteLine("The fees to be paid by the student is Rs{0:0.00}" + $"{total:0.00}");
+            }
+        }
+    }
+}
