32ef92899a9ed4b4bdc309e6758ed4138493e629
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-29 09:32:21 +0530

    Add files via upload

---
+++ Day 43/Sredha Krishna/27-Question1/27-Question1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int pos_count = 0;
+            int neg_count = 0;
+            int zero_count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    zero_count++;
+                }
+                else if (arr[i] > 0)
+                {
+                    pos_count++;
+                }
+                else if (arr[i] < 0)
+                {
+                    neg_count++;
+                }
+            }
+            double pos_ratio = (double)pos_count / n;
+            double neg_ratio = (double)neg_count / n;
+            double zero_ratio = (double)zero_count / n;
+            Console.WriteLine(pos_ratio.ToString("F6"));
+            Console.WriteLine(neg_ratio.ToString("F6"));
+            Console.WriteLine(zero_ratio.ToString("F6"));
+        }
+
+    }
+}
---
+++ Day 43/Sredha Krishna/27-question3/27-question3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Result
+
+{
+    public static int Candles(List<int> candles)
+
+    {
+        int maxHeight = 0;
+        int count = 0;
+        foreach (int height in candles)
+
+        {
+            if (height > maxHeight)
+            {
+                maxHeight = height;
+                count = 1;
+            }
+
+            else if (height == maxHeight)
+            {
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+
+}
+
+class Solution
+
+{
+    public static void Main(string[] args)
+
+    {
+        Console.WriteLine("Enter size");
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+        Console.WriteLine("enter elements");
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+        int result = Result.Candles(candles);
+        Console.WriteLine(result);
+
+    }
+
+}
