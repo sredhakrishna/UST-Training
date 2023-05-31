f52183555a5ed69cc984a7b768db28c06ea737c4
sredhakrishna <90337653+sredhakrishna@users.noreply.github.com>
2023-05-31 14:55:23 +0530

    Add files via upload

---
+++ Day 44/Sredha Krishna/question 3/question 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+namespace Challenge3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            BigInteger a;
+            BigInteger b;
+            Console.WriteLine("Enter number 1:");
+            a = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number2 :");
+            b = BigInteger.Parse(Console.ReadLine());
+            BigInteger c = a + b;
+            BigInteger d = a * b;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
